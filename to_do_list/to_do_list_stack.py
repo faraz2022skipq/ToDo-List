bf017bcd7f38d12a6aefb984d2f7aca332b8cb23
Muhammad <muhammad.faraz.skipq@gmail.com>
2022-12-20 17:40:37 +0500

    User pool client

--- to_do_list/to_do_list_stack.py
+++ to_do_list/to_do_list_stack.py
@@ -10,6 +10,8 @@ class ToDoListStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_cognito/UserPool.html
+        # Creating user pool
         user_pool = cognito.UserPool(self, "todopool",
             user_pool_name = "todopool",
             removal_policy = RemovalPolicy.DESTROY,
@@ -19,6 +21,20 @@ class ToDoListStack(Stack):
             password_policy = cognito.PasswordPolicy(min_length=6),
             account_recovery = cognito.AccountRecovery.EMAIL_ONLY
             )
+        
+        # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_cognito/UserPoolClient.html
+        # App Client for userpools
         user_pool_client = cognito.UserPoolClient(self, "todopoolclient"
             user_pool = user_pool,
+            auth_flows={
+                'admin_user_password': True,
+                'user_password': True,
+                'custom': True,
+                'user_srp': True},
+            # Authentication flows allow users on a client to be authenticated with a user pool.
+            o_auth = cognito.OAuthSettings(flows = cognito.OAuthFlows(implicit_code_grant = True),
+                callback_urls = "http://localhost:8000/logged_in.html",
+                logout_urls = "http://localhost:8000/logged_in.html")
+            supported_identity_providers = [cognito.UserPoolClientIdentityProvider.COGNITO],
+            user_pool_client_name = "todopoolclient"
             )
